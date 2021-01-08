6d03740559602e206ff4ef3ae6bddc66b4724a4b
Mikhail Zakharov <mizak@envisage.no>
2021-01-08 14:49:26 +0100

    feat(database): add database functionality
    NOTE: Does not compile because ChatSession does not have the required
    methods yet.

--- app/src/main/java/bavard/chat/Message.java
+++ app/src/main/java/bavard/chat/Message.java
@@ -2,15 +2,21 @@ package bavard.chat;
 
 import bavard.user.User;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class Message {
 
-    private User from;
-    private User to;
-    private LocalDateTime date; //private Timestamp date;
+    private User sender;
+    private User recipient;
+    private OffsetDateTime datetime;
 
-    public Message(User from, User to, LocalDateTime date) {
-
+    protected Message(User from, User to, OffsetDateTime when) {
+        sender = from;
+        recipient = to;
+        datetime = when;
     }
+
+    public User getSender() { return sender; }
+    public User getRecipient() { return recipient; }
+    public String getDatetime() { return datetime.toString(); }
 }
--- app/src/main/java/bavard/chat/TextMessage.java
+++ app/src/main/java/bavard/chat/TextMessage.java
@@ -2,14 +2,16 @@ package bavard.chat;
 
 import bavard.user.User;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class TextMessage extends Message {
 
     private String text;
 
-    public TextMessage(User from, User to, LocalDateTime date, String text){
-        super(from, to, date);
+    public TextMessage(User from, User to, OffsetDateTime when, String text){
+        super(from, to, when);
         this.text = text;
     }
+
+    public String getText() { return text; }
 }
